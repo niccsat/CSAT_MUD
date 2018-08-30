c368a4723435af01c32f51f3b79f9447d0b4b07b
TrevorMech <42850644+TrevorMech@users.noreply.github.com>
2018-08-30 16:16:48 -0700

    Added more parameters to rooms.

--- run.py
+++ run.py
@@ -32,10 +32,14 @@ rooms = {
     "Tavern": {
         "description": "You're in a cozy tavern warmed by an open fire.",
         "exits": {"outside": "Outside"},
+        "items": ["Blaster","StimPack"],
+        "tools": ["Sink","Towel"],
     },
     "Outside": {
         "description": "You're standing outside a tavern. It's raining.",
         "exits": {"inside": "Tavern"},
+        "items":[],
+        "tools":[],
     }
 }
 
