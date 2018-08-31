4a0d27efb242fa846efbd3a03c69a15219dfdfcb
TrevorMech <42850644+TrevorMech@users.noreply.github.com>
2018-08-31 12:13:38 -0700

    Added items.py and rooms.py for easier development.

---
+++ items.py
@@ -0,0 +1,12 @@
+items = {
+    "Ray Gun": {
+        "description": "Don't use without adult supervision!",
+        "damage": 10,
+        "value": 75,
+    },
+    "Titanium Bar": {
+        "description": "An ingot of very strong metal.",
+        "damage": 1,
+        "value": 25,
+        
+}
