0562e4945dd8813a8b3d771a5e7e8ef51ae83d73
TrevorMech <42850644+TrevorMech@users.noreply.github.com>
2018-10-09 11:59:06 -0700

    Added template NPCs

--- npcs.py
+++ npcs.py
@@ -1,4 +1,14 @@
 npcs = {
+    "store template": {
+        "quote": "I say this when you interact with me!",
+        "type": "shop", # i am a shop
+        "items": {"item for sale": 500},  # second param is cost, in credits
+    },
+    "dialogue template": {
+        "quote": "This is what I say!",
+        "type": "dialogue", # i am a talker
+        "quote2": "I say this second!",
+        },
     "melon shop 1": {
         "quote": "Yummy Watermelon for Sale!",
         "type": "shop",
